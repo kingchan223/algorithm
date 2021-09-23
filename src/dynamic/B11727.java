72c6d2f918db6760e4fadebe93791f7e2999e321
kingchan223 <kokiyo97@naver.com>
2021-09-23 15:11:23 +0900

    다이나믹 프로그래밍 기본 문제 풀기

---
+++ src/dynamic/B11727.java
@@ -0,0 +1,20 @@
+package dynamic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B11727 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[] memo = new long[n + 1];
+        memo[1] = 1;
+        if(n>1) memo[2] = 3;
+
+        for(int i=3; i<=n; i++){
+            memo[i] = (memo[i - 1] + ((memo[i - 2])*2))%10007;/* 왜 여기서 나머지를 구하면 맞고,*/
+        }
+        System.out.println(memo[n]);/* 여기서 나머지를 memo[n]%10007로 하면 틀린지 모르겠음*/
+    }
+}
--- src/dynamic/B1463.java
+++ src/dynamic/B1463.java
@@ -5,13 +5,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class B1463 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] memo = new int[n + 1];
-        memo[1] = 0;
-        memo[2] = 1;
-        memo[3] = 1;
 
+        memo[1] = 0;
+        if(n>1) memo[2] = 1;
+        if(n>2) memo[3] = 1;
+
+        for(int i=4; i<=n; i++){
+            memo[i] = memo[i-1] + 1;
+            if(i%2==0 && memo[i/2] + 1 < memo[i])
+                memo[i] = memo[i/2] + 1;
+            if(i%3==0 && memo[i/3] + 1 < memo[i])
+                memo[i] = memo[i/3] + 1;
+        }
+
+        System.out.println(memo[n]);
     }
 }
