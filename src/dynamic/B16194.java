1de87273ee81291897cc549a42d367a27722e71e
kingchan223 <kokiyo97@naver.com>
2021-09-26 15:40:03 +0900

    실패한 DP다시 풀기

--- src/dynamic/B16194.java
+++ src/dynamic/B16194.java
@@ -4,21 +4,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-//i개의 최대값을 구하려면 i-1개, i-2개... 들의 최대값을 구해놔야한다. -> 이것이 DP
-public class B11052 {
+
+public class B16194 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int[] card = new int[n + 1];
         int[] result = new int[n + 1];
+        for (int i=1 ; i<=n; i++) {
+            result[i]=1000*10000;
+        }
         int index = 1;
+
         while(st.hasMoreTokens())
             card[index++] = Integer.parseInt(st.nextToken());
 
         for(int i=1; i<=n; i++){
             for (int j = 1; j <= i; j++) {
-                result[i] = Math.max(result[i], card[j] + result[i - j]);//j+ i-j 는 i개이다.
+                result[i] = Math.min(result[i], card[j] + result[i - j]);
             }
         }
         System.out.println(result[n]);
--- src/dynamic/B9095.java
+++ src/dynamic/B9095.java
@@ -9,13 +9,13 @@ public class B9095 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] memo = new int[11];
+        memo[0] = 1;
         memo[1] = 1;
         memo[2] = 2;
-        memo[3] = 4;
 
         while(n-->0){
             int num = Integer.parseInt(br.readLine());
-            for(int i=4; i<=num; i++){
+            for(int i=3; i<=num; i++){
                 memo[i] = memo[i-3] + memo[i-2] + memo[i-1];
             }
             System.out.println(memo[num]);
