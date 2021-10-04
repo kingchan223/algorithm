816e98402e70c0321325ae75143246c2397cb62e
kingchan223 <kokiyo97@naver.com>
2021-10-05 00:32:16 +0900

    DP문제 풀기

--- src/dynamic/B1912.java
+++ src/dynamic/B1912.java
@@ -4,46 +4,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+/*새로운 배열. 즉 dp배열을 만들 생각을 하자.*/
 public class B1912 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int[] arr = new int[n];
+        int[] d = new int[n];
         for(int i=0; i<n; i++){
             int num = Integer.parseInt(st.nextToken());
             arr[i] = num;
         }
-        int result = -1000;
-        int temp = 0;
-
-
-        for (int i = 0; i < n; i++) {
-            if(temp < temp + arr[i]) temp += arr[i];
-            else temp = 0;
-            result = Math.max(temp, result);
+        int result = arr[0];
+        d[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            if(arr[i]+d[i-1] > arr[i]) d[i] = arr[i]+d[i-1];
+            else d[i] = arr[i];
+            if( result < d[i]) result = d[i];
         }
-
-//        for (int i = 0; i < n; i++) {
-//            if(arr[i] < 0){
-//                temp +=
-//            }
-//                temp += arr[i];
-//            if(temp < result) temp = 0;
-//            result = Math.max(temp, result);
-//        }
-        // 10 -4 3 1 5 6 -35 12 21 -1
-        // 2 1 -4 3 4 -4 6 5 -5 1
-        // -1 -2 -3 -4 -5
-
-//        int front = 0;
-//        int back = arr[0];
-//        for (int i = 0; i < n; i++) {
-//            front += arr[i];
-//            if(front < back) temp = back;
-//            result = Math.max(temp, result);
-//        }
         System.out.println(result);
     }
 }
